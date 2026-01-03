ff6964d5b7462b73626ec8f066d73526ad870c95
vanyagift <243577664+1fair-labs@users.noreply.github.com>
2026-01-03 21:55:37 +0300

    Disable redirect from root to /landing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import Landing from "./pages/Landing";
 import MiniApp from "./pages/MiniApp";
